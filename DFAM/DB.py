d3003553a93fdda9215bf6fe680b5aec5632a247
shwarzes89 <shwarzes89@gmail.com>
2015-05-12 15:38:03 +0900

    remove swipe feature because lack of method invocation timestamp

--- DFAM/DB.py
+++ DFAM/DB.py
@@ -4,16 +4,8 @@ DBFile = "BadUIDB"
 class DBHelper(object):
     conn = sqlite3.connect(DBFile)
 
-    def writeCOORD(timestamp, x, y):
-    
-        def getTimestampSeq():
-            return conn.execute('SELECT seq_id, time_stamp from SeqTable;').fetchall()
-        
-        seqs = getTimestampSeq()
-
-        filter(lambda x : x[0] < )
-
-        conn.execute('INSERT INTO COORDTable(x,y) VALUES(?, ?)', (x,y))
+    def writeCOORD(x, y):
+        conn.execute('INSERT INTO SeqTable(x,y) VALUES(?, ?)', (x,y))
         conn.commit()
 
     def writeMethodCall(transition):
