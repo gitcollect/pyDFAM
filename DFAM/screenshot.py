642e05741971dbd3c822c1533d3b58b5b0ab65c4
Kyeong Wook Ma <shwarzes89@gmail.com>
2015-08-03 01:02:24 +0900

    screenshot test done

--- DFAM/screenshot.py
+++ DFAM/screenshot.py
@@ -1,7 +1,13 @@
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice, MonkeyImage
+import sys
 
 device = MonkeyRunner.waitForConnection()
 
-def snap(usr_id, timestamp):
+def main():
+    usr_id = sys.argv[1]
+    timestamp = sys.argv[2]
     screenshot = device.takeSnapshot()
     screenshot.writeToFile(usr_id + " "  + timestamp,'png')
+
+if __name__ == "__main__":
+    main()
